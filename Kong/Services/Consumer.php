af753ccbd2120fea53ea0838a8e59925a2e1e36e
Ranjani Renganathan <ranjani.renganathan@shipwire.com>
2017-11-27 16:31:42 -0800

    limiting the change

--- Kong/Services/Consumer.php
+++ Kong/Services/Consumer.php
@@ -67,31 +67,31 @@ class Consumer implements ConsumerInterface
 
     public function allPlugins($consumer, $body = [])
     {
-        return $this->client->get('consumers/'.$consumer.'/plugins', ['body' => $body]);
+        return $this->client->get('/consumers/'.$consumer.'/plugins', ['body' => $body]);
     }
 
     public function createPlugin($consumer, $body = [])
     {
-        return $this->client->post('consumers/'.$consumer.'/plugins', ['body' => $body]);
+        return $this->client->post('/consumers/'.$consumer.'/plugins', ['body' => $body]);
     }
 
     public function deletePlugin($consumer, $id)
     {
-        return $this->client->delete('consumers/'.$consumer.'/plugins/'.$id);
+        return $this->client->delete('/consumers/'.$consumer.'/plugins/'.$id);
     }
 
     public function allAcls($consumer, $body = [])
     {
-        return $this->client->get('consumers/'.$consumer.'/acls', ['body' => $body]);
+        return $this->client->get('/consumers/'.$consumer.'/acls', ['body' => $body]);
     }
 
     public function createAcl($consumer, $body = [])
     {
-        return $this->client->post('consumers/'.$consumer.'/acls', ['body' => $body]);
+        return $this->client->post('/consumers/'.$consumer.'/acls', ['body' => $body]);
     }
 
     public function deleteAcl($consumer, $id)
     {
-        return $this->client->delete('consumers/'.$consumer.'/acls/'.$id);
+        return $this->client->delete('/consumers/'.$consumer.'/acls/'.$id);
     }
 }
